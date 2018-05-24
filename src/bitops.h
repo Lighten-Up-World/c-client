a8a7a981f099700e0b4980f39f9b1ff1be92a529
Matt Malarkey <mm5917@imperial.ac.uk>
2018-05-24 22:15:22 +0100

    Header file bitops.h created. Needs review of what functions are actually needed

---
+++ src/bitops.h
@@ -0,0 +1,20 @@
+//
+// Created by Matt Malarkey on 24/05/2018.
+//
+
+#ifndef ARM11_22_BITOPS_H
+#define ARM11_22_BITOPS_H
+
+#include "arm.h"
+
+// Copy bits x to y from inst to ext
+// Pre: x >= y
+// Pre: 0 <= x,y <= length(instr)
+// Pre: ext must be of size y - x + 1
+void getBits(word_t *inst, int *ext, int x, int y);
+
+// Get the shifted 32 bit integer from the full instruction
+// Pre: instr must be DP format
+val_t getDataOp2(word_t instr);
+
+#endif ARM11_22_BITOPS_H
