21ccca8974f146ff7bfb855a15a49105955465b2
Daniel Hails <daniel@hails.info>
2018-05-25 14:49:24 +0100

    Add custom error conditions/codes and setup shorthand macros for failures

---
+++ src/error.h
@@ -0,0 +1,37 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+#include <errno.h>
+
+typedef enum {
+  EC_OK,
+  EC_INVALID_PARAM,
+  EC_SYS
+} error_code;
+
+typedef struct {
+  int code;
+  const char *message;
+} error_str;
+
+const error_str error_strs[] = {
+  {EC_OK, "No error encountered."},
+  {EC_INVALID_PARAM, "Invalid parameter."}
+};
+
+// Convert error codes to and from system ones
+#define EC_FROM_SYS_ERROR(e) (EC_SYS + e)
+#define EC_TO_SYS_ERROR(e) (e - EC_SYS)
+#define EC_IS_SYS_ERROR(e) (e >= EC_SYS)
+
+//DO-While allows macro to resemble single statement
+#define FAIL_PRED(pred, status) \
+  do {if (pred) {_status = status; goto fail;}} while(0)
+#define FAIL_SYS(pred) \
+  do {if (pred) {_status = EC_FROM_SYS_ERROR(errno); goto fail;}} while(0)
+#define FAIL_FORWARD(expr) \
+  do {_status = expr; if (_status != EC_OK) goto fail;} while(0)
+
+void ec_strerror(const int status, char *buffer, const size_t buffer_length);
+
+#endif
