b478ca8e3d8baa84b766ebd5b77c821fe6d12d3d
Matt Malarkey <mm5917@imperial.ac.uk>
2018-05-25 14:44:15 +0100

    Compile and run shell script added for convenience, .gitignore updated, bitops.c in progress

--- src/bitops.c
+++ src/bitops.c
@@ -1,2 +1,35 @@
 // TODO: Extract bits x to y from 32 bit instruction
 // TODO: Make 32 bit integer from 8 bit for shifting
+
+#include <stdlib.h>
+#include <assert.h>
+#include "bitops.h"
+
+// Main for testing
+int main(int argc, char **argv) {
+
+  word_t zero = 0;
+  word_t num = 5;
+
+  assert(zero == logicalShiftLeft(zero, 0));
+  assert(zero == logicalShiftLeft(zero, 4));
+  assert(zero == logicalShiftLeft(zero, 72));
+
+  assert(num == logicalShiftLeft(num, 0));
+  assert(10 == logicalShiftLeft(num, 1));
+  assert(40 == logicalShiftLeft(num, 3));
+
+  return 0;
+}
+
+
+//zero fill from right, discard carry
+word_t logicalShiftLeft(word_t value, byte_t shift) {
+  return value << shift;
+}
+
+word_t logicalShiftRight(word_t value, byte_t shift);
+
+word_t arithShiftRight(word_t value, byte_t shift);
+
+word_t rotateRight(word_t value, byte_t shift);
--- src/bitops.h
+++ src/bitops.h
@@ -3,7 +3,8 @@
 //
 
 // TODO: Extract bits x to y from 32 bit instruction
-// TODO: Make 32 bit integer from 8 bit for shifting
+// TODO: Make 32 bit integer from 8 bit for shifting (0 pad)
+// TODO: Add all shifting operations
 
 #ifndef ARM11_22_BITOPS_H
 #define ARM11_22_BITOPS_H
@@ -16,8 +17,18 @@
 // Pre: ext must be of size y - x + 1
 void getBits(word_t *inst, int *ext, int x, int y);
 
-// Get the shifted 32 bit integer from the full instruction
-// Pre: instr must be DP format
-val_t getDataOp2(word_t instr);
+word_t bits(word_t *inst, int x, int y);
 
-#endif ARM11_22_BITOPS_H
+word_t logicalShiftLeft(word_t value, byte_t shift);
+
+word_t logicalShiftRight(word_t value, byte_t shift);
+
+word_t arithShiftRight(word_t value, byte_t shift);
+
+word_t rotateRight(word_t value, byte_t shift);
+
+// see Op2 on page7
+// left pad with 0s
+word_t leftPadZeros(byte_t);
+
+#endif
