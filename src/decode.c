0c22b36947019998ff259e6cccb28b2ad3710fb3
user <wb2117@ic.ac.uk>
2018-05-25 14:40:39 +0100

    Added entry point for decode

--- src/decode.c
+++ src/decode.c
@@ -1,4 +1,6 @@
 #include "arm.h"
+#include <assert.h>
+#include <stdio.h>
 
 //// DP ////
 
@@ -19,3 +21,19 @@
 
 
 //// DECODE ENTRY ////
+
+/*
+ * @params - word_t word is the binary instruction to decode
+ * @return - instruction_t* is a pointer to the decoded instruction
+ */
+
+instruction_t* decodeWord(word_t word){
+    assert(word != NULL);
+
+    instruction_t instruction;
+    instruction_t* instructionPtr = &instruction;
+
+    //Call functions to decode instruction
+
+    return instructionPtr;
+}
