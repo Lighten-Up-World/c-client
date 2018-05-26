4bf24d251eb0897294ff4973f5e0f4017f83a964
Daniel Hails <daniel@hails.info>
2018-05-26 19:07:50 +0100

    Rephrase  functions in execute to take their respective functions

--- src/execute.c
+++ src/execute.c
@@ -1,4 +1,44 @@
 #include "arm.h"
+#include "bitops.h"
+/**
+* Checks if the condition on the decoded instruction is met using the current
+* state of the flags register (CPSR).
+*
+* @param state The current VM state
+* @returns 1 when condition is met, 0 if not.
+*/
+int condition(state_t *state, condition_t cond){
+  byte_t flags = getNibble(state->registers.cpsr, sizeof(word_t));
+
+  switch(cond){
+    case EQ:
+      return ;
+    case NE:
+      return ;
+    case GE:
+      return ;
+    case LT:
+      return ;
+    case GT:
+      return ;
+    case LE:
+      return ;
+    case AL:
+      return ;
+    default:
+      fprintf(stderr, "Incorrect cond flag %x\n", cond);
+      //free_state();
+      return 0;
+  }
+}
+
+void execute(state_t *state);
+void executeDPI(state_t *state);
+void executeMUL(state_t *state);
+void executeBRN(state_t *state);
+void executeSDT(state_t *state);
+void executeHAL(state_t *state);
+
 
 //// DP ////
 
--- src/execute.h
+++ src/execute.h
@@ -3,13 +3,13 @@
 
 #include "arm.h"
 
-int condition(state_t *state);
+int condition(state_t *state, condition_t cond);
 
 void execute(state_t *state);
-void executeDPI(state_t *state);
-void executeMUL(state_t *state);
-void executeBRN(state_t *state);
-void executeSDT(state_t *state);
-void executeHAL(state_t *state);
+void executeDPI(state_t *state, dp_instruction_t  instr);
+void executeMUL(state_t *state, mul_instruction_t instr);
+void executeBRN(state_t *state, brn_instruction_t instr);
+void executeSDT(state_t *state, sdt_instruction_t instr);
+void executeHAL(state_t *state, hal_instruction_t instr);
 
 #endif
