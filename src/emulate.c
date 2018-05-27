3edeab64596d4c929c31d1c4c93782fae6949794
Daniel Hails <daniel@hails.info>
2018-05-27 03:50:17 +0100

    Emulate loop finished

--- src/emulate.c
+++ src/emulate.c
@@ -1,8 +1,30 @@
 #include <stdlib.h>
 #include "arm.h"
+#include "io.h"
+#include "execute.h"
+#include "register.h"
+
 
 int main(int argc, char **argv) {
-  //TODO: Setup default state
-  //TODO: Pipeline loop
+  assert(argc > 1);
+
+  state_t *state = calloc(1, sizeof(state_t));
+  state.pipeline.decoded = calloc(1, sizeof(instruction_t));
+  readFile(argv[1], state->memory, MEM_SIZE);
+
+  //Setup Pipeline
+  setPC(state, 0x8);
+  state->pipeline.decoded = decode(state->memory[0]);
+  state->pipeline.fetched = state->memory[1];
+
+  while(state->pipeline.decoded->type != HAL){
+    execute(state);
+    state->pipeline.decoded = decode(state->pipeline.fetched);
+    state->pipeline.fetched = state->memory[getPC(state) >> 2];
+    incrementPC(state);
+  }
+  execute(state); // Execute HAL instruction
+  free(state.pipeline.decoded);
+  free(*state);
   return EXIT_SUCCESS;
 }
