1400d3adb24142fffc39eea08bf9b76447d258ef
Daniel Hails <daniel@hails.info>
2018-05-29 14:39:08 +0100

    Download all test cases

--- test/lib/testsuite.rb
+++ test/lib/testsuite.rb
@@ -15,8 +15,8 @@ class TestSuite
     @test_cases_path       = File.absolute_path(test_cases_path)
     @emulate_assemble_path = File.absolute_path(emulate_assemble_path)
 
-    @emulate  = File.absolute_path(File.join(emulate_assemble_path,"emulate"))
-    @assemble = File.absolute_path(File.join(emulate_assemble_path,"assemble"))
+    @emulate  = File.absolute_path(File.join(emulate_assemble_path,"emulate.out"))
+    @assemble = File.absolute_path(File.join(emulate_assemble_path,"assemble.out"))
 
     sources
 
