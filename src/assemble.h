4e4e9a87fdeb1aabb4ce2864e55e9521f45d22de
Daniel Hails <daniel@hails.info>
2018-05-30 23:58:53 +0100

    Couple of files for assemble

--- src/assemble.h
+++ src/assemble.h
@@ -1,6 +1,23 @@
 #ifndef ASSEMBLE_H
 #define ASSEMBLE_H
 
+typedef struct {
+  word_t word;
+  address_t address;
+} bin_instr_t;
 
+typedef struct {
+  bin_instr_t **instrs;
+  symbol_map_t *sym_m;
+  reference_map_t *ref_m;
+  int lines;
+  word_t mPC;
+} program_t;
+
+program_t *program_new();
+int program_delete(program_t *);
+int program_add_symbol(program_t *, label_t, address_t);
+int program_add_reference(program_t *, label_t, address_t);
+void program_toString(program_t *, char *);
 
 #endif
---
+++ src/tokenizer.h
@@ -0,0 +1,35 @@
+#ifndef TOKENIZER_H
+#define TOKENIZER_H
+
+typedef enum{
+  T_OPCODE,
+  T_REGISTER,
+  T_HASH_EXPR,
+  T_EQ_EXPR,
+  T_SHIFTNAME,
+  T_L_BRACKET,
+  T_R_BRACKET,
+  T_COMMA,
+  T_MINUS,
+  T_PLUS,
+}
+
+typedef struct {
+  token_type_t type;
+  char *str;
+} token_t;
+
+/**
+* Takes a source string, one character separator and a pointer to an external
+* character separator used for returning sub strings.
+*
+* @param src: the source string
+* @param sep: the char separator
+* @param ouput: the place in memory to store the separated sublists
+* @returns The number of tokens found
+*/
+int str_separate(char *src, char sep, char ***output);
+
+int tokenize(char *line, token_t *out);
+
+#endif
