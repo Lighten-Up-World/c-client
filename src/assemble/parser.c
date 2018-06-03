808bde822d80c6fc27efe3d3bfda6659012d0236
Matt Malarkey <mm5917@imperial.ac.uk>
2018-06-03 13:41:28 +0100

    parse_dp getting there, lots of small syntactic error needs fixing before moving on

--- src/assemble/parser.c
+++ src/assemble/parser.c
@@ -1,26 +1,46 @@
-#include <utils/instructions.h>
+#include <memory.h>
+#include <complex.h>
 #include "tokenizer.h"
 #include "../utils/instructions.h"
 
 int consume_token(token_t *arr, token_type_t type);
 
-int parse_dp(token_t *tkns, instruction_t *inst) {
-  inst->type = DP;
-  inst->i = {
-      padding,
-      I,
-      opcode,
-      S,
-      rn,
-      rd,
-      operand2
-  };
-  return 0;
-}
-
 char remove_first_char(char *string) {
   return *(string + 1);
 }
+
+int parse_dp(token_t *tokens, instruction_t *inst) {
+  inst->type = DP;
+
+  const char *opcode[] = tokens[0].str;
+
+  reg_address_t rn;
+  reg_address_t rd;
+
+  // Set Rd as second token
+  rd = (reg_address_t) remove_first_char(*tokens[1].str); //cast to integer for reg-address
+
+  // Set whether the CPSR flags should be set
+  bool S;
+  if (strcmp("tst", opcode) == 0 || strcmp("teq", opcode) == 0 || strcmp("cmp", opcode) == 0) {
+    S = 1;
+    rn = (reg_address_t) remove_first_char(*tokens[1].str);
+  } else {
+    S = 0;
+  }
+
+  inst->i.dp.padding = 0x00;
+  inst->i.dp.I = I;
+  inst->i.dp.opcode = opcode; //do we have to use the enum for this
+  inst->i.dp.S = S;
+  inst->i.dp.rn = rn;
+  inst->i.dp.rd = rd;
+  inst->i.dp.operand2 = operand2;
+
+  return 0;
+}
+
+
 
 int parse_mul(token_t *tokens, instruction_t *inst) {
   inst->type = MUL;
@@ -28,44 +48,44 @@ int parse_mul(token_t *tokens, instruction_t *inst) {
   flag_t A;
   flag_t S = false;
 
-  //check these work
+  //check these work - need to cast to integer from string first
   reg_address_t rd = (reg_address_t) remove_first_char(*tokens[1].str);
   reg_address_t rn = (reg_address_t) remove_first_char(*tokens[2].str);
   reg_address_t rs = (reg_address_t) remove_first_char(*tokens[3].str);
   reg_address_t rm = 0;
 
-  if (*tokens[0].str == "mul") {
+  if (strcmp(*tokens[0].str, "mul") == 0) {
     A = 0;
   } else {
     A = 1;
     rm = (reg_address_t) remove_first_char(*tokens[0].str);
   }
 
-  inst->i = (mul_instruction_t) {
-      (byte_t) 0x0,
-      A,
-      S,
-      rd,
-      rn,
-      rs,
-      (byte_t) 0x1001,
-      rm
-  };
+  inst->i.mul.pad0 = (byte_t) 0x0;
+  inst->i.mul.A = A;
+  inst->i.mul.S = S;
+  inst->i.mul.rd = rd;
+  inst->i.mul.rn = rn;
+  inst->i.mul.rs = rs;
+  inst->i.mul.pad9 =(byte_t) 0x1001;
+  inst->i.mul.rm = rm;
+
   return 0;
 }
 
 int parse_sdt(token_t *tokens, instruction_t *inst) {
   inst->type = SDT;
-  inst->i = {
-      0x01,
-      I,
-      P,
-      U,
-      0x0,
-      L,
-      rn,rd,
-      offset
-  };
+
+  inst->i.sdt.pad0 = 0x01;
+  inst->i.sdt.I = I;
+  inst->i.sdt.P = P;
+  inst->i.sdt.U = U;
+  inst->i.sdt.pad0 = 0x0;
+  inst->i.sdt.L = L;
+  inst->i.sdt.rn = rn;
+  inst->i.sdt.rd = rd;
+  inst->i.sdt.offset = offset;
+
   return 0;
 }
 
@@ -84,15 +104,14 @@ int parse_brn(token_t *tokens, instruction_t *inst) {
       break;
     default:
       //handle error here
+      break;
   }
 
   // encode offset
 
+  inst->i.brn.padA = (byte_t) 0x1010;
+  inst->i.brn.offset = offset;
 
-  inst->i = {
-      0x1010,
-      offset
-  };
   return 0;
 }
 
@@ -108,10 +127,9 @@ int parse_lsl(token_t *tokens, instruction_t *inst) {
   return -1;
 }
 
-
 int parse_halt(token_t *tokens, instruction_t *inst) {
   inst->type = HAL;
-  inst->i = {0u};
+  inst->i.hal.pad0 = 0u;
   return 0;
 }
 
@@ -120,11 +138,11 @@ void parse_label();
 
 // TODO: find out how to use the map and convert this
 typedef struct {
-  char op[];
+  char *op[];
   int (*parse_func) (token_t*, instruction_t*);
 } op_to_parser;
 
-const NUM_NON_BRANCH_OPS = 16;
+const int NUM_NON_BRANCH_OPS = 16;
 const op_to_parser oplist[] = {
     {"add", parse_dp},
     {"sub", parse_dp},
@@ -166,7 +184,7 @@ int parse(token_t *tokens, instruction_t *inst) {
   char *opcode[] = tokens[0].str;
 
   // Parse a branch instruction and its condition
-  if (*opcode[0] == 'b') {
+  if (strcmp(*opcode[0], 'b') == 0) {
     parse_brn(tokens, inst);
     return 0;
   }
@@ -177,7 +195,7 @@ int parse(token_t *tokens, instruction_t *inst) {
   // Calculate function pointer to parse an instruction from the opcode
   // TODO: find out how to use the map and convert this
   for (int i = 0; i < NUM_NON_BRANCH_OPS; i++) {
-    if (oplist[i].op == *opcode) {
+    if (strcmp(oplist[i].op, *opcode) == 0) {
       return oplist[i].parse_func(tokens, inst);
     }
   }
