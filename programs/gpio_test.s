943dae86aa14174ed144963d59373660da284511
Matt Malarkey <mm5917@imperial.ac.uk>
2018-06-07 00:05:03 +0100

    more gpio tests added, still not working

--- programs/gpio_test.s
+++ programs/gpio_test.s
@@ -6,9 +6,15 @@ str r1,[r0,#4]
 
 mov r1,#1
 lsl r1,#16
-str r1,[r0,#40]
+str r1,[r0,#0x28]
 
-str r1,[r0,#28]
+str r1,[r0,#0x1C]
+
+mov r1,#1
+lsl r1,#20
+wait:
+sub r1,r1,#1
+cmp r1,#1
+bne wait
 
 andeq r0,r0,r0
-
