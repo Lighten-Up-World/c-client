53846232112ba81a893c8d0dc57d1933ef899140
djh17 <daniel.hails17@imperial.ac.uk>
2018-06-14 02:57:30 +0100

    Header outlines for effect/effect_mixer/opc

---
+++ extension/src/opc_client.h
@@ -0,0 +1,29 @@
+#ifndef OPC_CLIENT_T
+#define OPC_CLIENT_T
+
+#include <stdint.h>
+
+typedef struct opc_client opc_client_t;
+typedef struct opc_header opc_header_t;
+
+struct opc_client{
+  int fd;
+  char *host;
+  char *address;
+};
+
+struct opc_header{
+  uint8_t channel;
+  uint8_t command;
+  uint8_t length[2];
+};
+
+opc_header_t *opc_header_init(uint8_t channel, uint8_t command, uint8_t length);
+uint8_t *opc_header_data(opc_header_t *self);
+
+int opc_connect(opc_client_t* self);
+int opc_close(opc_client_t* self);
+int opc_resolve(opc_client_t* self, const char *hostport, int defaultPort);
+int opc_write(const uint8_t *data, size_t length);
+
+#endif
