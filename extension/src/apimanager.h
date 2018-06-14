dbafec7b4ade3bd97ea94de3ebfb17789cd93322
user <wb2117@ic.ac.uk>
2018-06-14 13:15:26 +0100

    Replace jsmn with parson library, change parsed value to double

--- extension/src/apimanager.h
+++ extension/src/apimanager.h
@@ -21,7 +21,7 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <netdb.h>
-#include "jsmn/jsmn.h"
+#include "parson/parson.h"
 #include "pixel.h"
 
 //OPEN_WEATHER_MAP
@@ -37,7 +37,7 @@
 typedef struct geolocation{
     int latitude;
     int longitude;
-    int value;
+    double value;
 } geolocation_t;
 
 typedef struct location_map{
@@ -59,7 +59,7 @@ typedef struct http_request{
 
 int get_value_for_geolocation(geolocation_t *loc, char *host, char *path ,char *attr);
 
-int get_int_from_json(char *buf, char *name, int *val);
+int get_double_from_json(char *buf, char *name, double *val);
 
 int send_get_request(int sock, http_request_t request, char *buf, size_t buf_size);
 
