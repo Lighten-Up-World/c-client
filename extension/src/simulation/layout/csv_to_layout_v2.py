db288a4bc398488f9b9b1d616b4848361367a843
Matt Malarkey <mm5917@imperial.ac.uk>
2018-06-15 14:12:14 +0100

    csv_v2 now writes to json layout file also

--- extension/src/simulation/layout/csv_to_layout_v2.py
+++ extension/src/simulation/layout/csv_to_layout_v2.py
@@ -1,6 +1,8 @@
 import csv
 
+spacing = 0.10
 mappings = []
+pixels = []
 with open('WorldMap.csv', 'rb') as csvfile:
     reader = csv.reader(csvfile)
     y = 0
@@ -13,6 +15,8 @@ with open('WorldMap.csv', 'rb') as csvfile:
             if not cell:
                 #ie. no pixel in this cell
                 continue
+            pixels.append('{"point": [%.2f, %.2f, %.2f]}' %
+                          (x * spacing, 0, y * spacing))
             mappings.append('%d %d %d' % (x, y, count))
             count+=1
     with open('CoordsToListPos.txt', 'w') as ci:
