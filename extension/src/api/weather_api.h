b5834d75cbbab5be788218aabec756d845346fca
Daniel Hails <daniel@hails.info>
2018-06-16 23:16:06 +0100

    Add Spare api key

--- extension/src/api/weather_api.h
+++ extension/src/api/weather_api.h
@@ -13,6 +13,7 @@
 #define WEATHER_HOST "api.openweathermap.org"
 #define WEATHER_PATH "data/2.5/weather?lat=%f&lon=%f&appid=6ee4372288ed6d49c7dea5ed1f39a118"
 
+#define DANIEL_OPENWEATHERMAP_API_KEY b9442812ef8ec5f8f52d4cca809b36b9
 //TEMPERATURE
 int temp_get_pixel_for_xy(pixel_t *pixel, geolocation_t geoloc);
 
