eae85c5159c246c8ee373e1064acf947613e5ed5
Daniel Hails <daniel@hails.info>
2018-06-16 20:09:04 +0100

    Generalise to pixel_info struct

--- extension/src/main.c
+++ extension/src/main.c
@@ -24,20 +24,14 @@
 #define NUM_PIXELS 471
 
 #define PIXEL_FILE "layout/CoordsToListPos.txt"
-#define GEOLOC_FILE "layout/GeoLocToCoords.txt"
+#define GEOLOC_FILE "layout/GeoLocToListPos.txt"
 
 #define HOST_AND_PORT "127.0.0.1:7890"
 
 typedef struct {
   grid_t grid;
-  int pos;
-} grid_pos_t;
-
-typedef struct {
-  grid_t grid;
   geolocation_t geo;
-} grid_geo_t;
-
+} pixel_info_t;
 
 volatile int interrupted = 0;
 
@@ -45,33 +39,45 @@ void handle_user_exit(int _) {
   interrupted = 1;
 }
 
-int init_grid_pos(list_t* list){
+int init_grid(list_t* list){
   csv_parser_t *coords_to_pos_parser = csv_parser_new(PIXEL_FILE, " ");
   csv_row_t *row;
   while ((row = csv_parser_getRow(coords_to_pos_parser)) ) {
       char **rowFields = csv_parser_getFields(row);
-
-      grid_pos_t *pair = malloc(sizeof(grid_pos_t));
-      *pair = (grid_pos_t){{.x = atoi(rowFields[0]), .y =atoi(rowFields[1])},
-              .pos = atoi(rowFields[2])};
-      list_add(list, pair);
-
+      int pos = atoi(rowFields[2]);
+      pixel_info_t *pi;
+      if((pi = list_get(list, pos)) == NULL){
+        pi = malloc(sizeof(pixel_info_t));
+        *pi = (pixel_info_t){{.x = atoi(rowFields[0]), .y =atoi(rowFields[1])},
+                {-1, -1}};
+        list_add(list, pi); //TODO: NEEDS TO ADD IN CORRECT POS
+      }
+      else {
+        pi->grid = (grid_t){.x = atoi(rowFields[0]), .y =atoi(rowFields[1])};
+      }
       csv_parser_destroy_row(row);
   }
   csv_parser_destroy(coords_to_pos_parser);
   return 0;
 }
 
-int init_grid_geo(list_t* list){
+int init_geo(list_t* list){
   csv_parser_t *coords_to_pos_parser = csv_parser_new(GEOLOC_FILE, " ");
   csv_row_t *row;
   while ((row = csv_parser_getRow(coords_to_pos_parser)) ) {
       char **rowFields = csv_parser_getFields(row);
 
-      grid_geo_t *pair = malloc(sizeof(grid_geo_t));
-      *pair = (grid_geo_t){{.x = atoi(rowFields[2]), .y = atoi(rowFields[3])},
-              {.latitude = atof(rowFields[0]), .longitude = atof(rowFields[1])}};
-      list_add(list, pair);
+      int pos = atoi(rowFields[2]);
+      pixel_info_t *pi;
+      if((pi = list_get(list, pos)) == NULL){
+        pi = malloc(sizeof(pixel_info_t));
+        *pi = (pixel_info_t){{-1, -1},
+                {.latitude = atof(rowFields[0]), .longitude =atof(rowFields[1])}};
+        list_add(list, pi); //TODO: NEEDS TO ADD IN CORRECT POS
+      }
+      else {
+        pi->geo = (geolocation_t){.latitude = atof(rowFields[0]), .longitude =atof(rowFields[1])};
+      }
 
       csv_parser_destroy_row(row);
   }
@@ -94,28 +100,14 @@ int main(int argc, const char * argv[]) {
     pixels[p] = (pixel){PIXEL_COLOUR_MAX, PIXEL_COLOUR_MAX,PIXEL_COLOUR_MAX};
   }
 
-  FILE *file = fopen(PIXEL_FILE, "r");
-  if (file == NULL) {
-    perror("File could not be opened");
-    return -1;
-  }
-
-  FILE *loc_file = fopen(GEOLOC_FILE, "r");
-  if (loc_file == NULL) {
-    perror("File could not be opened");
-    return -1;
-  }
-
-  list_t *grid_pos = list_new(&free);
-  init_grid_pos(grid_pos);
-
-  list_t *grid_geo = list_new(&free);
-  init_grid_geo(grid_geo);
+  list_t *pixel_info = list_new(&free);
+  init_grid(pixel_info);
+  init_geo(pixel_info);
 
   for (int i = 0; i < NUM_PIXELS; i++) {
-    geolocation_t geo = ((grid_geo_t *)list_get(grid_geo, i))->geo;
+    geolocation_t geo = ((pixel_info_t *)list_get(pixel_info, i))->geo;
     pixel_t pix;
-    pix.grid = ((grid_pos_t *)list_get(grid_pos, i))->grid;
+    pix.grid = ((pixel_info_t *)list_get(pixel_info, i))->grid;
 
     if (strncmp("temp", argv[1], strlen(argv[1])) == 0){
       if(temp_get_pixel_for_xy(&pix, geo) < 0){
