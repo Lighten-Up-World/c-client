770077d98e88e870c01c7558fbdc983f697a8451
Matt Malarkey <mm5917@imperial.ac.uk>
2018-06-16 20:00:24 +0100

    Python csv modified to work with new WorldMap csv file (moved incraments to x, y to end of loop), pixels are now zero indexed as expected

--- extension/src/layout/csv_to_layout.py
+++ extension/src/layout/csv_to_layout.py
@@ -13,16 +13,17 @@ with open('WorldMap.csv', 'rb') as csvfile:
     count = 0
     for row in list(reader):
         x = 0
-        y+=1
         for cell in list(row):
-            x+=1
+
             if not cell:
                 #ie. no pixel in this cell
                 continue
             pixels.append('\t{"point": [%.2f, %.2f, %.2f]}' %
                           ((width - x + x_offset) * spacing, 0, (height - y + y_offset) * spacing))
             mappings.append('%d %d %d' % (x, y, count))
-            count+=1
+            count += 1
+            x += 1
+        y += 1
     with open('CoordsToListPos.txt', 'w') as ci:
         ci.write('\n'.join(mappings))
     with open('WorldMap.json', 'w') as ci:
