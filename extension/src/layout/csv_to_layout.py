f14eae7857a622ab9d60e595139bee6ced98db8c
Daniel Hails <daniel@hails.info>
2018-06-19 23:35:05 +0100

    Add strip information to pixel_info

--- extension/src/layout/csv_to_layout.py
+++ extension/src/layout/csv_to_layout.py
@@ -8,7 +8,7 @@ width = 53
 height = 24
 x_offset = -26
 y_offset = -12
-with open('layout/csv/Wiring.csv', 'rb') as csvfile:
+with open('layout/csv/WorldMap.csv', 'rb') as csvfile:
     reader = csv.reader(csvfile)
     y = 0
     count = 0
@@ -23,14 +23,13 @@ with open('layout/csv/Wiring.csv', 'rb') as csvfile:
                           ((width - x + x_offset) * spacing, 0, (height - y + y_offset) * spacing))
             mappings.append('%d %d %d' % (x, y, count))
             cell = str(cell).split("-");
-            print(cell)
-            strip.append('%s %s %d' % (cell[0], cell[1], count))
+            strip.append('%d %s %d' % (int(cell[0])+1, cell[1], count))
             count+=1
             x += 1
         y += 1
     with open('layout/strip_config.txt', 'w') as sf:
         sf.write('\n'.join(strip))
-    with open('layout/CoordsToListPos.txt', 'w') as ci:
+    with open('layout/coordinates.txt', 'w') as ci:
         ci.write('\n'.join(mappings))
     with open('layout/WorldMap.json', 'w') as ci:
         ci.write('[\n' + ',\n'.join(pixels) + '\n]');
