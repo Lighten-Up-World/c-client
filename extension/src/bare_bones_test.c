ede36c3b532bbeaecd24a22fdc4e2f3a4107cc43
Matt Malarkey <mm5917@imperial.ac.uk>
2018-06-21 13:04:42 +0100

    bare_bones_test added

---
+++ extension/src/bare_bones_test.c
@@ -0,0 +1,22 @@
+#include <signal.h>
+#include "opc/opc_client.h"
+
+#define DIM_PIXEL (opc_pixel_t) {50,50,50}
+
+int main() {
+  opc_sink sink;
+
+  // Open connection
+  sink = opc_new_sink(HOST_AND_PORT);
+  if (sink == -1) {
+    exit(EXIT_FAILURE);
+  }
+
+
+  opc_pixel_t *pixels = calloc(NUM_PIXELS, sizeof(opc_pixel_t));
+  for (int i = 0; i < NUM_PIXELS; i++) {
+    pixels[i] = DIM_PIXEL;
+  }
+
+  opc_put_pixels(sink, 0, NUM_PIXELS, pixels);
+}
