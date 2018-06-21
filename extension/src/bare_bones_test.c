bd02c7ac66e2f14fb6ce97526cf160d905a2aee1
Matt Malarkey <mm5917@imperial.ac.uk>
2018-06-21 13:44:47 +0100

    Updated json config file

--- extension/src/bare_bones_test.c
+++ extension/src/bare_bones_test.c
@@ -12,11 +12,20 @@ int main() {
     exit(EXIT_FAILURE);
   }
 
-
   opc_pixel_t *pixels = calloc(NUM_PIXELS, sizeof(opc_pixel_t));
   for (int i = 0; i < NUM_PIXELS; i++) {
-    pixels[i] = (opc_pixel_t) {0, 0, 255};
+    pixels[i] = (opc_pixel_t) {50, 50, 50};
   }
+
+  struct timespec ts;
+  ts.tv_sec = 1 / 1000;
+  ts.tv_nsec = (1 % 1000) * 1000000;
+
+  for (int x = 0; x < NUM_PIXELS; x++) {
+    pixels[x] = WHITE_PIXEL;
+    nanosleep(&ts, NULL);
+  }
+
 
   opc_put_pixels(sink, 0, NUM_PIXELS, pixels);
 }
