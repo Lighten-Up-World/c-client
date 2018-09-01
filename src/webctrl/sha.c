44dd35f9c214a76071325e26d54e3638a3becbd0
Matt Malarkey <mm5917@imperial.ac.uk>
2018-09-01 17:52:24 +0100

    sha.c now has a base64 encode function, and produces the correct hashed output for a given Sec-WebSocket-Key

--- src/webctrl/sha.c
+++ src/webctrl/sha.c
@@ -5,8 +5,38 @@
 #include <openssl/sha.h>
 #include "ctrlserver.h"
 
+#include <openssl/bio.h>
+#include <openssl/evp.h>
+#include <openssl/buffer.h>
+#include <stdint.h>
+
+// TODO: figure out how this works and implement error checking?
+// https://gist.github.com/barrysteyn/7308212
+
+int Base64Encode(const unsigned char* buffer, size_t length, char** b64text) { //Encodes a binary safe base 64 string
+  BIO *bio, *b64;
+  BUF_MEM *bufferPtr;
+
+  b64 = BIO_new(BIO_f_base64());
+  bio = BIO_new(BIO_s_mem());
+  bio = BIO_push(b64, bio);
+
+  BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL); //Ignore newlines - write everything in one line
+  BIO_write(bio, buffer, length);
+  BIO_flush(bio);
+  BIO_get_mem_ptr(bio, &bufferPtr);
+  BIO_set_close(bio, BIO_NOCLOSE);
+  BIO_free_all(bio);
+
+  *b64text=(*bufferPtr).data;
+
+  return (0); //success
+}
+
+// TODO: base64 decode and check against test inputs
+
 int main() {
-  char *in = "Sec-WebSocket-Key: keyherelololol\r\n";
+  char *in = "Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\n";
 
   // Extract client hash
   char *key_header_start = strstr(in, WEBSOCKET_KEY_HEADER);
@@ -35,11 +65,22 @@ int main() {
 
   unsigned char *hash = calloc(20, sizeof(char));
   SHA1((const unsigned char *)key, strlen((const char *) key), hash);
-  for (int i = 0; i < 20; i++) {
-    printf("%02x", hash[i]);
-  }
-  puts("");
 
+  hash[20] = '\0';
+  printf("hash: %s\n", hash);
+
+  // base64 encode the output
+  char *s2;
+  Base64Encode(hash, strlen((const char *) hash), &s2);
+
+  printf("base64: %s\n", s2);
+
+  // should return s3pPLMBiTxaQ9kYGzzhZRbK+xOo=
+  char *s1 = "s3pPLMBiTxaQ9kYGzzhZRbK+xOo=";
+  //char *s2 = (char *) b64;
+  if (strncmp(s1, s2, strlen(s1)) == 0) {
+    puts("NAILED IT");
+  }
 
   return 0;
 }
