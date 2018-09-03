62dfe458b7bfa4045da1762f41b6ffd5f2198002
Matt Malarkey <mm5917@imperial.ac.uk>
2018-09-03 18:23:23 +0100

    Testing file to make sure hashes are processed correcty

--- src/webctrl/sha.c
+++ src/webctrl/sha.c
@@ -8,11 +8,12 @@
 #include <openssl/evp.h>
 #include <openssl/buffer.h>
 #include <stdint.h>
+#include "ctrlserver.h"
 
 // TODO: figure out how this works and implement error checking?
 // https://gist.github.com/barrysteyn/7308212
 
-int Base64Encode(const unsigned char* buffer, size_t length, char** b64text) { //Encodes a binary safe base 64 string
+int Base64Encode(const unsigned char* buffer, int length, char** b64text) { //Encodes a binary safe base 64 string
   BIO *bio, *b64;
   BUF_MEM *bufferPtr;
 
@@ -32,52 +33,53 @@ int Base64Encode(const unsigned char* buffer, size_t length, char** b64text) { /
   return 0;
 }
 
-int main() {
-  char *in = "Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\n";
-
-  // Extract client hash
-  char *key_header_start = strstr(in, WEBSOCKET_KEY_HEADER);
-  if (key_header_start == NULL) {
-    perror("Sec-WebSocket-Key not present");
-    exit(errno);
-  }
-
-  char *key_start = strchr(key_header_start, ' ') + 1;
-  if (key_start == NULL || key_start != key_header_start + strlen(WEBSOCKET_KEY_HEADER)) {
-    perror("Sec-WebSocket-Key header is malformed (key start)");
-    exit(errno);
-  }
-
-  char *key_end = strchr(key_start, '\r');
-  if (key_end == NULL) {
-    perror("Sec-WebSocket-Key header is malformed (key end)");
-    exit(errno);
-  }
-
+void calc_hash(char *key) {
   // Append magic string to the client provided hash, then take the sha1 hash to send in response
-  size_t key_len = key_end - key_start;
-  char *key = calloc(key_len + strlen(SEC_WEBSOCKET_MAGIC), sizeof(char));
-  memcpy(key, key_start, key_len);
-  memcpy(key + key_len, SEC_WEBSOCKET_MAGIC, strlen(SEC_WEBSOCKET_MAGIC));
-
-  unsigned char *hash = calloc(20, sizeof(char));
-  SHA1((const unsigned char *)key, strlen((const char *) key), hash);
-
-  hash[20] = '\0';
-  printf("hash: %s\n", hash);
-
-  // base64 encode the output
-  char *s2;
-  Base64Encode(hash, strlen((const char *) hash), &s2);
-
-  printf("base64: %s\n", s2);
-
-  // should return s3pPLMBiTxaQ9kYGzzhZRbK+xOo=
-  char *s1 = "s3pPLMBiTxaQ9kYGzzhZRbK+xOo=";
-  //char *s2 = (char *) b64;
-  if (strncmp(s1, s2, strlen(s1)) == 0) {
-    puts("NAILED IT");
+  char *to_hash = calloc(strlen(key) + strlen(SEC_WEBSOCKET_MAGIC), sizeof(char));
+  if (to_hash == NULL) {
+    perror("Hash--alloc");
+    exit(errno);
   }
+  strncpy(to_hash, key, strlen(key));
+  strncat(to_hash, SEC_WEBSOCKET_MAGIC, strlen(SEC_WEBSOCKET_MAGIC));
+
+  unsigned char *sha1 = calloc(21, sizeof(char));
+  if (sha1 == NULL) {
+    perror("Hash alloc");
+    exit(errno);
+  }
+  SHA1((const unsigned char *)to_hash, strlen((const char *) to_hash), (unsigned char *) sha1);
+  sha1[20] = '\0';
+
+  size_t len = strlen((const char *) sha1);
+  if (len != 20) {
+    printf("sha1:  %zu\n", len);
+  }
+
+  char *base64;
+  Base64Encode((const unsigned char *) sha1, (int) strlen((const char *) sha1), &base64);
+  printf("base64: %s\n", base64);
+}
+
+int main() {
+  FILE *f = fopen("/Users/Matt/Projects/ARM Project/c-client/keys_filt.txt", "r");
+
+  size_t len = strlen("3Q7rCgkquNBD3DgkdYDl5Q==");
+  char *key = calloc(len + 2, sizeof(char)); // account for '\r\n' at end of line
+  char *hash = calloc(21, sizeof(char));
+  if (key == NULL || hash == NULL) {
+    perror("hash, key");
+    exit(errno);
+  }
+  int n = 0;
+  while (fgets(key, (int) (len + 3), f) != NULL && n < 15) {
+    //overwrite '\n' with '\0'
+    key[len] = '\0';
+    calc_hash(key);
+    n++;
+  }
+
+  fclose(f);
 
   return 0;
 }
