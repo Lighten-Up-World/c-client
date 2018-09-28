a8750b5b79d3fc7769da4ad52a5579760844a46b
Daniel Hails <daniel@hails.info>
2018-09-28 15:47:51 +0100

    Add demo script that executes effects in sequence

---
+++ src/demo.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# A simple demo script that executes effects in sequence, should be precompiled
+# and the server running
+
+## declare an array variable
+runner="./build/effect_runner.out"
+declare -a arr=("$runner temp_timelapse" "$runner temp" "$runner windspeed")
+delay=5
+
+while true
+do
+  for i in "${arr[@]}"
+  do
+     echo ""
+     echo "============================="
+     echo "Running $i for $delay seconds"
+     echo "============================="
+     echo ""
+     echo ""
+     # Launch script in background
+     $i > /dev/null &
+     # Get its PID
+     PID=$!
+     # Wait for 2 seconds
+     sleep $delay
+     # Kill it
+     kill $PID
+  done
+done
