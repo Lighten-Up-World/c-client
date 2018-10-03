07cb1b902e7967440717e2565304aad11989445e
Matt Malarkey <mm5917@imperial.ac.uk>
2018-10-03 17:46:51 +0100

    Remove webctrl folder, move ctrlserver to src/. Refactor effect_runner main loop ready to implement ctrlserver integration

--- src/effect_runner.c
+++ src/effect_runner.c
@@ -74,6 +74,23 @@ void effect_runner_delete(effect_runner_t *self) {
   free(self);
 }
 
+// TODO: change this to generic init, abstract set effect
+effect_t *init_effect(const char *arg, void *pixel_info, opc_sink sink) {
+  effect_t *effect = NULL;
+  for (int i = 0; i < sizeof(effects) / sizeof(string_to_constructor); i++) {
+    if (strcmp(effects[i].str, arg) == 0) {
+      effect = effects[i].new(pixel_info);
+    }
+  }
+  if (effect == NULL) {
+    fprintf(stderr, "Failed to construct effect using argument %s\n", arg);
+    opc_close(sink);
+    list_delete(pixel_info);
+    exit(EXIT_FAILURE);
+  }
+  return effect;
+}
+
 int init_grid(list_t *list) {
   csv_parser_t *coords_to_pos_parser = csv_parser_new(PIXEL_FILE, " ");
   csv_row_t *row;
@@ -141,14 +158,12 @@ int server_has_input(int server) {
   return 1;
 }
 
-int run_effect(const char *effect_arg) {
+// TODO: add generic cleanup code
+int main(int argc, const char *argv[]) {
+  assert(argc > 1);
 
-  // Check for aliased commands
-  for (int i = 0; i < sizeof(cmds) / sizeof(string_to_string); i++) {
-    if (strcmp(cmds[i].key, effect_arg) == 0) {
-      return system(cmds[i].value);
-    }
-  }
+  // Set up Ctrl+C handle
+  signal(SIGINT, handle_user_exit);
 
   // Open connection
   opc_sink sink = opc_new_sink(HOST_AND_PORT);
@@ -166,24 +181,16 @@ int run_effect(const char *effect_arg) {
   init_geo(pixel_info);
   init_strip(pixel_info);
 
-  // Find effect from argument
-  effect_t *effect = NULL;
-  for (int i = 0; i < sizeof(effects) / sizeof(string_to_constructor); i++) {
-    if (strncmp(effects[i].str, effect_arg, strlen(effects[i].str)) == 0) {
-      effect = effects[i].new(pixel_info);
-    }
-  }
-  if (effect == NULL) {
-    fprintf(stderr, "Failed to construct effect using argument %s\n", effect_arg);
-    opc_close(sink);
-    list_delete(pixel_info);
-    exit(EXIT_FAILURE);
-  }
+  // Set up control server
+  int server = 1;
+
+  // Initialise the effect
+  effect_t *effect = init_effect(argv[1], pixel_info, sink);
 
   // Initialise api manager
   effect_runner_t *effect_runner = effect_runner_init(NULL, effect, pixel_info, sink);
   if (effect_runner == NULL) {
-    fprintf(stderr, "Api_manager failed to initialise with effect %s\n", effect_arg);
+    fprintf(stderr, "Api_manager failed to initialise with effect %s\n", argv[1]);
     free(effect);
     opc_close(sink);
     list_delete(pixel_info);
@@ -200,72 +207,16 @@ int run_effect(const char *effect_arg) {
     nanosleep(&effect_runner->effect->time_delta, NULL);
     effect_runner->effect->run(effect_runner);
     effect_runner->frame_no++;
+
+    // TODO: Handle server input
+    if (server_has_input(server)) {
+      argv[1] = "sun";
+      goto end;
+    }
   }
 
+end:
   // Close it all up
   effect_runner_delete(effect_runner);
   return EXIT_SUCCESS;
-}
-
-int web_server(void) {
-  // Create listening socket, setup server
-  ctrl_server *server = start_server();
-  char *read;
-  printf("Server started on %s\n", LISTEN_PORT);
-  // Simulates main loop of effect runner
-  while (!interrupted) {
-
-    // *** Effect runner code here ***
-    //printf("Looking for a client with %i\n", server->client_fd);
-    // If client is connected
-    if (server->client_fd) {
-      read = read_ws_frame(server);
-      run_effect(read);
-      free(read);
-    } else {
-      // Check if any client is waiting to connect
-      server->client_fd = try_accept_conn(server);
-
-      // If a connection was accepted, block until it's upgraded it to a WebSocket
-      if (server->client_fd) {
-        ssize_t read_size;
-        int tries = 0;
-        char *http_buffer = calloc(HTTP_BUFFER + 1, sizeof(char));
-        do {
-          read_size = get_latest_input(server, http_buffer, HTTP_BUFFER);
-          tries++;
-        } while (read_size < 0 && tries < 5);
-
-        if (read_size > 0) {
-          puts("Upgrading to WebSocket...");
-          if (try_to_upgrade(server, http_buffer)) {
-            close_client(server);
-          }
-        } else {
-          close_client(server);
-        }
-
-        free(http_buffer);
-      }
-
-    }
-    sleep_for(1);
-  }
-
-  close_server(server);
-
-  return EXIT_SUCCESS;
-}
-
-int main(int argc, const char *argv[]) {
-  assert(argc > 1);
-
-  // Set up Ctrl+C handle
-  signal(SIGINT, handle_user_exit);
-
-  if (strcmp(argv[1], "-ws") == 0) {
-    web_server();
-  }
-
-  run_effect(argv[1]);
 }
