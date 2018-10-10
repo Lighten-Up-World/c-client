6964efc8bba95a4ce52d44b0f79d41cbd868eaf2
RPI <mm5917@ic.ac.uk>
2018-10-10 18:57:42 +0000

    Fix demo.sh, all working

--- src/demo.sh
+++ src/demo.sh
@@ -5,18 +5,19 @@
 cd /home/pi/c-client/src
 
 runner="build/effect_runner.out"
-delay=5
+delay=20
 basic_cmd_port=9091
 
-$runner &
+$runner >/dev/null &
 
 while true
 do
     # Currently commands has 7 items
-    for i in {8..9};
+    for i in {0..5};
     do
         # Open netcat connection and send a command
         sleep $delay
-        echo $i | nc -N localhost $basic_cmd_port
+        echo "Playing effect number: " $i
+        echo $i | nc -w0 localhost $basic_cmd_port
     done
 done
